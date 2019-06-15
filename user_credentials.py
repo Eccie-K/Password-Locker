85763b78e0d367c5460f13a6b173c3f5204c497c
esther <estherkc100@gmail.com>
2019-06-15 19:28:49 +0300

    changed user file name

---
+++ user_credentials.py
@@ -0,0 +1,14 @@
+class User:
+    """
+Class that generates new instances of credentials
+    """
+pass
+
+def _init_(self, first_name, last_name,password):
+    """
+   _init_  Method that helps us define properties for our objects
+   """
+   #instance variables
+    self.first_name = first_name
+    self.last_name = last_name
+    self.password  =  password
