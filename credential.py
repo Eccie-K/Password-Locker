ffc2f3fe51d8985348212f15c4a583727279a60a
esther <estherkc100@gmail.com>
2019-06-16 15:16:42 +0300

    tested credential code on credential test file

---
+++ credential.py
@@ -0,0 +1,41 @@
+class Credential:
+    """"
+    A class that generates new instances of credentials
+    """
+    credential_list = []
+
+
+    def __init__(self, account, password):
+        '''
+        __init__method that helps us with the properties of the  user object
+        Args:
+            account: new credential account
+            password: new credential password
+        '''
+        self.account = account
+        self.password = password
+
+
+    def save_credential(self):
+            '''
+            save_credential method saves credential objects into credential_list
+            '''
+            Credential.credential_list.append(self)
+
+    @classmethod
+    def display_credentials(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.credential_list
+
+
+    # def confirm_credential(cls, username, password):
+    #         '''
+    #         Method that checks if the account and password  entered match entries in the credential_list
+    #         '''
+    #         current_credential = ''
+    #         for credential in Credential.credential_list:
+    #             if (credential.account == account and credential.password == password):
+    #                 current_user = credential.username
+    #         return current_credential
