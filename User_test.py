f29cf7f4258ec97b318dd3af88d745ce6f1bdc95
esther <estherkc100@gmail.com>
2019-06-16 12:50:12 +0300

    tested code for  User class

--- User_test.py
+++ User_test.py
@@ -1,22 +1,70 @@
-import unittest # importing the unittest module
-from user_credentials import User # importing the user class
+import unittest
 
-class  TestUser(unittest.TestCase):
-    '''
-    Test class that defines test cases for the user class behaviours
-    Args:
-        unittest.TestCase: TestCase class that helps in creating testcases
-    '''
+from user import User
+#  import user
+
+class TestUser(unittest.TestCase):
+    # setup and class creation above
     def setUp(self):
         '''
-        setUp method to run before each test cases
+        Function to help create user account details before each test
         '''
-        self.new_user = User("Ess", "Lay", "passwd") # create user object
+        self.new_user = User("Esther", "23489", "estherkc100@gmail.com")
 
     def test_init(self):
         '''
-        test_init test case to test if the object initialized properly
+        test_init test case to test if the object is initialized properly
         '''
+        self.assertEqual(self.new_user.username,"Esther")
+        self.assertEqual(self.new_user.password,"23489")
+        self.assertEqual(self.new_user.email,"estherkc100@gmail.com")
+
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user() # saving a new user
+        self.assertEqual(len(User.user_list),1)
+
+    # setup and class creation above
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
+
+    def test_save_multiple_user(self):
+        '''
+        test_save_multiple_user test checks to test if the user can save multiple users to our user_list
+        '''
+        self.new_user.save_user()
+        test_user = User("Esther", "23489", "estherkc100@gmail.com") # new user
+        test_user.save_user()
+        self.assertEqual(len(User.user_list),2)
+
+    def test_confirm_user(self):
+        '''
+        Function to confirm login details to current user
+        '''
+        self.new_user = User("Esther", "23489", "estherkc100@gmail.com")
+        self.new_user.save_user()
+        test_user= User("Esther", "23489", "estherkc100@gmail.com")
+        test_user.save_user()
+        active_user = User.confirm_user("Esther", "23489", "estherkc100@gmail.com")
+        self.assertTrue(active_user)
+
+    def test_display_all_user(self):
+        '''
+        method that returns a list of all users saved
+        '''
+
+        self.assertEqual(User.display_user(),User
+        .user_list)
+
+if __name__ == '__main__':
+    unittest.main()
+        
    
 
 
--- user.py
+++ user.py
@@ -1,16 +1,44 @@
 class User:
     """
-Class that generates new instances of users
+    class that generates new instance of users
     """
-    user_list = [] #empty user list
+    
+    user_list = []  # empty Users list
+
+    def save_user(self):
+        '''
+        save_user method that saves user objects into user_list
+        '''
+        User.user_list.append(self)
+
+    
+
+    def __init__(self, username, password, email):
+        '''
+        defining the structure of the user object
+        '''
+
+        self.username = username
+        self.password = password
+        self.email = email
+
+    @classmethod
+    def confirm_user(cls, username, password, email):
+        '''
+        Method to check if the name, password and email entered match entries in the users_list
+        '''
+        current_user = ''
+        for user in User.user_list:
+            if (user.username == username and user.password == password):
+                current_user = user.username
+        return current_user
+
+    @classmethod
+    def display_user(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.user_list
 
 
-def _init_(self, first_name, last_name,password):
-    """
-   _init_  Method that helps us define properties for our objects
-   """
-   #instance variables
-    self.first_name = first_name
-    self.last_name = last_name
-    self.password  =  password
     
